3115572169f8679136cede4a5dc1adf31b320058
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-01 16:39:34 -0500

    create initial repo and imports players

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: "alice",
+          score: 1001
+        },
+        {
+          id: 2,
+          name: "bob",
+          score: 20
+        },
+        {
+          id: 3,
+          name: "carol",
+          score: 500
+        }
+      ]
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
